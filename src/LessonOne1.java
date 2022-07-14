9246451e143753f0e733c306c7a7f3b3c02b1007
PavliukOleksandr <pavliuk.oleksandr.b@gmail.com>
2022-07-14 22:17:40 +0300

    first commit

---
+++ src/LessonOne1.java
@@ -0,0 +1,15 @@
+public class LessonOne1 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 2;
+        int result;
+        result = a +b;
+        System.out.println(result);
+        result = a-b;
+        System.out.println(result);
+        result = a*b;
+        System.out.println(result);
+        result = a/b;
+        System.out.println(result);
+    }
+}
